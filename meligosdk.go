162ba59521e90f6ba05fb77be49b29d2333dfd2b
Ahcobos <ahcobosp@gmail.com>
2016-07-21 02:00:05 -0500

    added MELI struct and base methods

---
+++ meligosdk.go
@@ -0,0 +1,36 @@
+package meligosdk
+
+import (
+  "fmt"
+  "io/ioutil"
+  http "net/http"
+)
+
+type Meli struct {
+  clientId  string
+  clientSecret string
+  redirectUri string
+  accesToken string
+  refreshToken string
+}
+
+func NewMeli(ClientId string, ClientSecret string, AccesToken string, RefreshToken string) Meli{
+  return Meli{
+    clientId: ClientId,
+    clientSecret: ClientSecret,
+    accesToken: AccesToken,
+    refreshToken: RefreshToken}
+}
+
+func NewMeliUsingAccesToken(ClientId string, ClientSecret string, AccesToken string) Meli{
+  return Meli{
+    clientId: ClientId,
+    clientSecret: ClientSecret,
+    accesToken: AccesToken}
+}
+
+func NewMeliUsingClientAndSecret(ClientId string, ClientSecret string) Meli{
+  return Meli{
+    clientId: ClientId,
+    clientSecret: ClientSecret}
+}
